c3e452344eca81fa1d698b6709481f72c3347f93
pankaj kumar <raipankaj150@gmail.com>
2021-12-02 23:27:15 +0530

    file

--- user_profile/src/controllers/user.controller.js
+++ user_profile/src/controllers/user.controller.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const fs = require('fs');
+
 
 const upload = require('../middleware/upload');
 const User = require('../models/user.model');
 const router = express.Router();
 
 
-router.post('/multiple', upload.any("profile_url"), async(req,res)=>{
+router.post('/multiple', upload.single("profile_url"), async(req,res)=>{
     try{
-        const filePaths = req.files.map(file=>file.path);
 
         const user = await User.create({
             first_name:req.body.first_name,
             last_name:req.body.last_name,
-            profile_pic:filePaths,
+            profile_pic:req.file.path,
         });
 
         res.send(user);
@@ -20,6 +21,55 @@ router.post('/multiple', upload.any("profile_url"), async(req,res)=>{
     catch(e){
         return res.status(500).send({status:"Failed",message:e.message});
     }
+});
+
+router.get('/',async (req,res)=>{
+    try{
+        const users = await User.find().lean().exec();
+
+        return res.send(users);
+    }
+    catch(e){
+        return res.status(500).send({status:"Failed",message:e.message});
+    }
 })
+
+router.patch('/multiple/:id', upload.single("profile_url"), async(req,res)=>{
+
+    
+    try{
+
+       const user  = await User.findById(req.params.id);
+       await fs.unlinkSync(user.profile_pic)
+ 
+       const newUser = await User.findByIdAndUpdate(req.params.id,{profile_pic:req.file.path},{new:true});
+
+        res.send(newUser);
+    }
+    catch(e){
+        return res.status(500).send({status:"Failed",message:e.message});
+    }
+});
+
+
+// need to modify delete
+router.delete('/multiple/:id', upload.single("profile_url"), async(req,res)=>{
+
+    
+    try{
+
+       const user  = await User.findById(req.params.id);
+       await fs.unlinkSync(user.profile_pic)
+ 
+       const newUser = await User.findByIdAndUpdate(req.params.id,{profile_pic:req.file.path},{new:true});
+
+        res.send(newUser);
+    }
+    catch(e){
+        return res.status(500).send({status:"Failed",message:e.message});
+    }
+});
+
+
 
 module.exports = router;
--- user_profile/src/models/user.model.js
+++ user_profile/src/models/user.model.js
@@ -3,7 +3,7 @@ const {Schema,model} = require('mongoose');
 const usersSchema = new Schema({
     first_name:{type:String, required:true},
     last_name:{type:String,required:true},
-    profile_pic:[{type:String, required:true}],
+    profile_pic:{type:String, required:true},
 },{
     versionKey:false,
     timestamps:true
