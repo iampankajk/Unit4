809dfb15cd2801e8f0a7ab1988979bba0563b756
pankaj kumar <raipankaj150@gmail.com>
2021-12-03 20:28:13 +0530

    file upload completed

--- user_profile/src/controllers/gallery.controller.js
+++ user_profile/src/controllers/gallery.controller.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const fs = require('fs');
 
 const upload = require('../middleware/upload');
 const Gallery = require('../models/gallery.model');
 const router = express.Router();
 
 
-router.post('/multiple', upload.any("pictures_urls"), async(req,res)=>{
+router.post('/multiple', upload.array("photos",5), async(req,res)=>{
     try{
+
         const filePaths = req.files.map(file=>file.path);
 
         const gallery = await Gallery.create({
@@ -19,6 +21,30 @@ router.post('/multiple', upload.any("pictures_urls"), async(req,res)=>{
     catch(e){
         return res.status(500).send({status:"Failed",message:e.message});
     }
-})
+});
+
+
+router.delete('/multiple/:id', async(req,res)=>{
+
+    
+    try{
+
+        const {pictures}  = await Gallery.findById(req.params.id);
+        
+        console.log(pictures)
+
+        pictures.forEach(async (pic)=>{
+           await fs.unlinkSync(pic)
+        });
+
+
+        res.send("del");
+    }
+    catch(e){
+        return res.status(500).send({status:"Failed",message:e.message});
+    }
+});
+
+
 
 module.exports = router;
