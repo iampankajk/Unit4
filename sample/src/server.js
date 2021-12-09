b4e8eba025227f94691462d8822ca72c24c60116
pankaj kumar <raipankaj150@gmail.com>
2021-12-09 23:05:05 +0530

    initial

---
+++ sample/src/server.js
@@ -0,0 +1,7 @@
+const app = require('./index');
+const connect = require('./configs/db');
+
+app.listen(4500,async (req,res)=>{
+    await connect();
+    console.log("app is listening on 4500");
+})
